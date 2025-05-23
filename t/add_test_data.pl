e347ecb347202336b59391e14df070639bd652b8
Tadeusz „tadzik” Sośnierz <tadeusz@sosnierz.com>
2025-05-23 12:46:53 +0200

    Have add_test_data create more thorough test data

--- t/add_test_data.pl
+++ t/add_test_data.pl
@@ -1,48 +1,32 @@
 use Modern::Perl;
 
-use CGI qw ( -utf8 );
-use HTML::Entities;
-use Try::Tiny;
-use C4::Context;
-use C4::Koha;
-use C4::Serials;    #uses getsubscriptionfrom biblionumber
-use C4::Output;
 use C4::Biblio;
-use C4::Items;
-use C4::Search;        # enabled_staff_search_views
-use C4::Tags qw(get_tags);
-use C4::XSLT;
-use Koha::DateUtils;
+use C4::Context;
 use Koha::Biblios;
-use Koha::Biblio;
-use Koha::Items;
-use Koha::ItemTypes;
-use Koha::Patrons;
-use Koha::Plugins;
-use Data::Dumper;
-use Text::CSV;
-use Getopt::Long;
-use Path::Tiny;
 
-my $testdata = [
-    [191, {'856_2' => 'IIIF-Manifest'}],
-    [5,   48, 16],
-    [11,  48.1, 16.1],
-];
-
-for my $p (@$testdata) {
-   biblio_add_manifests(@$p);
+my $dbh = C4::Context->dbh;
+for my $subfield (qw(2 a d)) {
+    $dbh->do(q(update marc_subfield_structure set hidden = 0 where tagfield = '856' and tagsubfield = ? and frameworkcode = 'BKS';), {}, $subfield);
 }
 
-sub biblio_add_manifests {
-   my ($biblionumber, $lat, $long) = @_;
-   my $biblio = Koha::Biblios->find($biblionumber);
-   my $framework = $biblio->frameworkcode;
-   my $record = $biblio->metadata->record;
-   my @fields;
-   $fields[0] = MARC::Field->new('856','','','2' => 'IIIF', 'd' => '0001.jpg');
-   $record->append_fields(@fields);
-   C4::Biblio::ModBiblio($record, $biblionumber, $framework);
-}
+# format: [$biblionumber, [fields: [args for MARC::Field]]]
+my @testdata = (
+    # 191: static manifest
+    [191, [ ['856','','','2' => 'IIIF-Manifest', 'd' => 'manifest.json'] ] ],
+    # 5: single image
+    [5,   [ ['856','','','2' => 'IIIF', 'd' => '0001.jpg'] ] ],
+    # 11: two images
+    [11,  [ ['856','','','2' => 'IIIF', 'd' => '0001.jpg'], ['856','','','2' => 'IIIF', 'd' => '0002.png'] ] ],
+);
 
-# http://kohadev.mydnsname.org:8080/cgi-bin/koha/opac-detail.pl?biblionumber=191
+for (@testdata) {
+    my ($biblionumber, $new_fields) = @$_;
+    my $biblio = Koha::Biblios->find($biblionumber);
+    my $framework = $biblio->frameworkcode;
+    my $record = $biblio->metadata->record;
+
+    my @fields = map { MARC::Field->new(@$_) } @$new_fields;
+
+    $record->append_fields(@fields);
+    C4::Biblio::ModBiblio($record, $biblionumber, $framework);
+}
