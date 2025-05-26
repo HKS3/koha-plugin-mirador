19a3ac170dab453d943efe7cd8387ab5950cdc29
Tadeusz „tadzik” Sośnierz <tadeusz@sosnierz.com>
2025-05-26 15:57:51 +0200

    Clean up Mirador.pm, remove unused code

--- Koha/Plugin/HKS3/Mirador.pm
+++ Koha/Plugin/HKS3/Mirador.pm
@@ -18,33 +18,17 @@ package Koha::Plugin::HKS3::Mirador;
 use Modern::Perl;
 
 use base qw(Koha::Plugins::Base);
-use Koha::Plugins::Tab;
 use C4::Biblio;
-use Koha::Biblios;
-use Koha::Items;
-use Cwd qw(abs_path);
 use C4::Context;
-use C4::Koha;
-use Koha::AuthorisedValues;
-use Data::UUID;
 use File::Slurp;
-use HTTP::Tiny;
+use Koha::Biblios;
+use Mojo::JSON qw(decode_json);
 use URI::Encode  qw(uri_encode);
 use Koha::Plugin::HKS3::IIIF qw(create_iiif_manifest);
-use Koha::Logger;
-
-use Mojo::JSON qw(decode_json);
-
-use Try::Tiny;
 
 =head1 Koha::Plugin::HKS3::Mirador
 
 A class implementing the controller code for Mirador requests
-
-=head2 Class methods
-
-=head3 method1
-
 
 =cut
 
@@ -137,26 +121,11 @@ sub configure {
     }   
 }
 
-
-
 sub api_namespace {
     my ( $self ) = @_;
 
     return 'hks3_mirador';
 }
-
-# Define a method to handle the IIIF manifest request
-sub handle_iiif_manifest {
-    my ($self, $cgi) = @_;
-    
-    # Assuming you have a way to get the bibliographic data and create a IIIF manifest
-    my $biblio_id = $cgi->param('biblio_id');
-    my $manifest = $self->create_iiif_manifest($biblio_id);
-    
-    print $cgi->header(-type => 'application/json', -charset => 'utf-8');
-    print encode_json($manifest);
-}
-
 
 # either get manifest from url or from file
 sub get_manifest {
@@ -250,24 +219,4 @@ sub opac_js {
     return "<script> $payload </script>";
 }
 
-
-sub opac_detail_xslt_variables {
-    my ( $self, $params ) = @_;
-
-    return { nice_message => 'We love Koha /' };
-}
-
-=head3 opac_results_xslt_variables
-
-Plugin hook injecting variables to the OPAC results XSLT
-
-=cut
-
-sub opac_results_xslt_variables {
-    my ( $self, $params ) = @_;
-
-    return { nice_message => 'We love Koha /' };
-}
-
 1;
-
