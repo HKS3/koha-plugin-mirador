def3dbeb08b0e7cd89d359adab4f47094dd4fc99
Tadeusz „tadzik” Sośnierz <tadeusz@sosnierz.com>
2026-01-06 14:43:24 +0100

    Libelle-specific changes

--- Koha/Plugin/HKS3/Mirador/opac.js
+++ Koha/Plugin/HKS3/Mirador/opac.js
@@ -1,52 +1,56 @@
 $(document).ready(function() {
     const mirador_page = $('body').attr('ID');
-    console.log(mirador_page);
     if (mirador_page == "opac-detail") {
         var x = document.getElementsByClassName("unapi-id")[0]
                     .getAttribute("title");
             biblionumber = x.split(':')[2];
-        addTab(biblionumber, 'opac');           
+        addMirador(biblionumber, 'opac');
     }
     else if (mirador_page == "catalog_detail") {
-        // console.log('alread set ',biblionumber); 
+        // console.log('alread set ',biblionumber);
         // intranet
-    } 
+    }
 
-    function addTab(biblionumber) {    
-        console.log('add tab');
-        var tab_classname = 'bibliodescriptions';   
-        
-        $("#tab_volumes").show();
-        
+    function addMirador(biblionumber) {
         $(function(e) {
             var ajaxData = { 'biblionumber': biblionumber };
             $.ajax({
-              url: '/api/v1/contrib/hks3_mirador/iiifmanifest',
-            type: 'GET',
-            dataType: 'json',
-            data: ajaxData,
-        })
-        .done(function(data) {
-            console.log('using mirador opac.js');            
-            var volumes_table =`<div class="row" id="mirador" style="width: 800px; height: 600px;"></div>`;
-            //var tabs = $('#'+tab_classname+' ul')
-            //    .append('<li id="tab_volumes"><a id="vol_label" href="#volumes">Volume</a></li>');
-            
-            // breadcrumbs document.querySelector("#breadcrumbs")
-            // var volumes = $("#breadcrumbs").after(volumes_table);                   
-            // var volumes = $("#catalogue_detail_biblio > div.record > h1")
-            
-            $("#catalogue_detail_biblio > div.record").append(`
-               <iframe src="/api/v1/contrib/hks3_mirador/iiifmanifest?biblionumber=`+biblionumber+`&viewer=1" width="800" height="600" 
-               marginwidth="0" marginheight="0" frameborder="0" scrolling="no" id="frame" allowfullscreen="">
-               <div id="mirador">Mirador Body</div>
-               </iframe>
-            `);                                 
-           
-        })   
-        .error(function(data) {
-            console.log('no data found');
-        });
+                url: '/api/v1/contrib/hks3_mirador/iiifmanifest',
+                type: 'GET',
+                dataType: 'json',
+                data: ajaxData,
+            })
+            .done(function(data) {
+                console.log('using mirador opac.js');
+                // $("#catalogue_detail_biblio > div.record").append(`
+                let fullUrl = `${window.location.origin}/api/v1/contrib/hks3_mirador/iiifmanifest?biblionumber=${biblionumber}`;
+                $("div.col-lg-3:has(> div#ulactioncontainer)").prepend(`
+                    <div width="100%" style="display: flex;">
+                        <input type="text"
+                            editable=false
+                            style="flex-grow: 1;"
+                            value="${fullUrl}"
+                        >
+                            <button type="button"
+                                onclick="navigator.clipboard.writeText('${fullUrl}')"
+                                class="btn btn-secondary"
+                            >
+                                Copy
+                            </button>
+                        </input>
+                    </div>
+                `);
+                $("div.col-lg-3:has(> div#ulactioncontainer)").prepend(`
+                   <iframe src="/api/v1/contrib/hks3_mirador/iiifmanifest?biblionumber=`+biblionumber+`&viewer=1"
+                   style='width: 100%; aspect-ratio: 4/3'
+                   marginwidth="0" marginheight="0" frameborder="0" scrolling="no" id="frame" allowfullscreen="">
+                   <div id="mirador">Mirador Body</div>
+                   </iframe>
+                `);
+            })
+            .error(function(data) {
+                console.log('no data found');
+            });
         });
     }
 })
